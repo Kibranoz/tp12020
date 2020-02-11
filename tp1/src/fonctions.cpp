5ca26f258eeefbda7bb5948a952fb7e81e65c0ee
Louis <louis.couture.3@ulaval.ca>
2020-02-11 09:58:45 -0500

    ajout des premiers fichiers

---
+++ tp1/src/fonctions.cpp
@@ -0,0 +1,15 @@
+/*
+ * fonctions.cpp
+ *
+ *  Created on: 2020-02-10
+ *      Author: etudiant
+ */
+#include <iostream>
+#include "fonctions.h"
+
+bool validerTelephone(const std::string& p_telephone){
+	//permet d'accéder à la variable en tant que telle (pas une copie?)
+	char listNumber[p_telephone.size()];
+
+}
+
---
+++ tp1/src/fonctions.h
@@ -0,0 +1,17 @@
+/*
+ * fonctions.h
+ *
+ *  Created on: 2020-02-10
+ *      Author: etudiant
+ */
+
+#ifndef SRC_FONCTIONS_H_
+#define SRC_FONCTIONS_H_
+#include <iostream>
+bool validerTelephone(const std::string& p_telephone);
+
+
+
+
+
+#endif /* SRC_FONCTIONS_H_ */
